8c96c4655b873c363f29bca83f607e9630738268
Owais  Khan <>
2021-06-01 07:14:03 +0000

    Starting to actively develop it

--- cogs/economy.py
+++ cogs/economy.py
@@ -2,7 +2,7 @@ import json
 import discord
 from discord.ext import commands
 
-
+main_colour = 0xb3701e
 async def getBankData():
   with open('cogs/json/bank.json','r') as f:
     data=json.load(f) 
@@ -27,16 +27,54 @@ class Economy(commands.Cog):
   async def on_ready(self):
     print(f"{self.qualified_name} is ready")
 
-  @commands.command(name='add')
+  @commands.command(name='deposit', aliases=['dep'])
   async def add(self,ctx, amount:int):
+    data = await getBankData()
     if amount<0:
-      pass
+      await ctx.send("Needs to be greater than 0")
+    elif amount==0:
+      await ctx.send("Needs to be greater than 0")
+    elif data[str(ctx.author.id)]["wallet"]<amount:
+      await ctx.send("You don't even have that much")
     else:
+      
+      if not str(ctx.author.id) in data:
+        await openAccount(ctx.author)
+        await ctx.send("You do not have a bank account. ONe has been opened for you.")
+      else:
+        data[str(ctx.author.id)]["bank"]+=amount
+        data[str(ctx.author.id)]["wallet"]-=amount
+        with open('cogs/json/bank.json','w') as f:
+          json.dump(data,f)
+        await ctx.send(f"Deposited {amount} successfully")
+
+  @commands.command(name='withdraw',aliases=['with'])
+  async def withdraw(self, ctx, amount:int):
+    data=await getBankData()
+    if not str(ctx.author.id) in data:
       await openAccount(ctx.author)
-      data=await getBankData()
-      data[str(ctx.author.id)]["wallet"]+=amount
-      with open('cogs/json/bank.json','w') as f:
-        json.dump(data, f)
+    wallet=data[str(ctx.author.id)]["wallet"]
+    bank = data[str(ctx.author.id)]["bank"]
+
+  @commands.command(name='balance',aliases=['bal'])
+  async def balance(self, ctx, member:discord.Member=None):
+    if member==None:
+      member=ctx.author
+    data=await getBankData()
+    if not str(member.id) in data: 
+      await openAccount(member)
+    else:
+      if member.name.startswith("_"):
+        name=f"\\{member.name}"
+      else:
+        name=member.name
+      wallet = data[str(member.id)]["wallet"]
+      bank = data[str(member.id)]["bank"]
+      embed=discord.Embed(title=f"Balance for {name}'s account", description="Here is your bank balance", embed=discord.Color.green())
+      embed.add_field(name="Bank balance: ", value=f"{bank}", inline = True)
+      embed.add_field(name="wallet balance: ", value=f"{wallet}", inline=True)
+      await ctx.send(embed=embed)
+
 
 def setup(client):
   client.add_cog(Economy(client))
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 import asyncio
 import datetime
 import praw
-from PIL import Image , ImageFont , ImageDraw
+from PIL import Image, ImageFont, ImageDraw
 from io import BytesIO
 import requests
 import asyncpraw
@@ -16,15 +16,18 @@ from aiohttp import ClientSession
 from prsaw import RandomStuff
 from webserver import keep_alive
 import statcord
+import sqlite3
 
 main_colour = 0xb3701e
 
+
 class EmbedHelpCommand(commands.HelpCommand):
 
-    COLOUR = main_colour 
+    COLOUR = main_colour
 
     def get_ending_note(self):
-        return 'Use {0}{1} [command] for more info on a command.'.format(self.clean_prefix, self.invoked_with)
+        return 'Use {0}{1} [command] for more info on a command.'.format(
+            self.clean_prefix, self.invoked_with)
 
     def get_command_signature(self, command):
         return '{0.qualified_name} {0.signature}'.format(command)
@@ -49,13 +52,16 @@ class EmbedHelpCommand(commands.HelpCommand):
         await self.get_destination().send(embed=embed)
 
     async def send_cog_help(self, cog):
-        embed = discord.Embed(title='{0.qualified_name} Commands'.format(cog), colour=self.COLOUR)
+        embed = discord.Embed(title='{0.qualified_name} Commands'.format(cog),
+                              colour=self.COLOUR)
         if cog.description:
             embed.description = cog.description
 
         filtered = await self.filter_commands(cog.get_commands(), sort=True)
         for command in filtered:
-            embed.add_field(name=self.get_command_signature(command), value=command.short_doc or '...', inline=False)
+            embed.add_field(name=self.get_command_signature(command),
+                            value=command.short_doc or '...',
+                            inline=False)
 
         embed.set_footer(text=self.get_ending_note())
         await self.get_destination().send(embed=embed)
@@ -68,49 +74,99 @@ class EmbedHelpCommand(commands.HelpCommand):
         if isinstance(group, commands.Group):
             filtered = await self.filter_commands(group.commands, sort=True)
             for command in filtered:
-                embed.add_field(name=self.get_command_signature(command), value=command.short_doc or '...', inline=False)
+                embed.add_field(name=self.get_command_signature(command),
+                                value=command.short_doc or '...',
+                                inline=False)
 
         embed.set_footer(text=self.get_ending_note())
         await self.get_destination().send(embed=embed)
 
     send_command_help = send_group_help
 
+
 def get_prefix(client, message):
     try:
         with open('prefixes.json', 'r') as f:
             prefixes = json.load(f)
             return prefixes[str(message.guild.id)]
-        
-    except KeyError: 
+
+    except KeyError:
         with open('prefixes.json', 'r') as k:
             prefixes = json.load(k)
         prefixes[str(message.guild.id)] = 'w!'
 
         with open('prefixes.json', 'w') as j:
-            json.dump(prefixes, j, indent = 4)
+            json.dump(prefixes, j, indent=4)
 
         with open('prefixes.json', 'r') as t:
             prefixes = json.load(t)
             return prefixes[str(message.guild.id)]
-        
-    except: 
-        return 'w!' 
+
+    except:
+        return 'w!'
+
 
 intents = discord.Intents.all()
-client = commands.Bot(intents=intents , help_command=EmbedHelpCommand() , command_prefix= (get_prefix))
+client = commands.Bot(intents=intents,
+                      help_command=EmbedHelpCommand(),
+                      command_prefix=(get_prefix))
 Color = discord.Colour.random()
 key = "7A8Dn3qoGL"
-rs = RandomStuff(async_mode=True , api_key = key)
-key = "statcord.com-ojBt9AMyayKIyQ6dhCt3"
-api = statcord.Client(client ,key)
+rs = RandomStuff(async_mode=True, api_key=key)
+key_statcord = "statcord.com-ojBt9AMyayKIyQ6dhCt3"
+api = statcord.Client(client, key_statcord)
 api.start_loop()
+conn = sqlite3.connect("cogs/dbs/main.db")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    else:
+        if message.content.lower().startswith(f'{get_prefix}'):
+            await client.process_commands(message)
+
+        cur = conn.cursor()
+        guildID = str(message.guild.id)
+        r = cur.execute("SELECT * FROM main WHERE guild_id = '" + guildID +
+                        "'")
+
+        row = None
+        for row in r:
+            ...
+
+        if row[2] == "0":
+            return
+
+        if row == None:
+            return
+
+        elif row != None:
+            if message.channel.id == int(row[1]):
+                async with message.channel.typing():
+                    try:
+                        msg = message.content
+                        if message.mentions != []:
+                            for i in message.mentions:
+                                msg = msg.replace(f'<@!{i.id}>', i.name)
+
+                        response = await rs.get_ai_response(msg)
+                    except json.decoder.JSONDecodeError:
+                        await asyncio.sleep(3)
+                        response = await rs.get_ai_response(msg)
+
+                await message.channel.send(response)
+
+            else:
+                return
+
 
 @client.event
 async def on_ready():
-    print ("Ready")
-    await client.change_presence(activity=discord.Game(f"w!help in {len(client.guilds)} servers"))
-
-
+    print("Ready")
+    await client.change_presence(
+        activity=discord.Game(f"w!help in {len(client.guilds)} servers"))
 
 
 for filename in os.listdir('./cogs'):
@@ -119,14 +175,14 @@ for filename in os.listdir('./cogs'):
 
 
 @client.event
-async def on_guild_join(guild): 
+async def on_guild_join(guild):
     with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f) 
+        prefixes = json.load(f)
 
     prefixes[str(guild.id)] = 'w!'
 
-    with open('prefixes.json', 'w') as f: 
-        json.dump(prefixes, f, indent=4) 
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
 
     name = str(guild.name)
     description = str(guild.description)
@@ -141,31 +197,27 @@ async def on_guild_join(guild):
     embed = discord.Embed(
         title=" Joined a server",
         description=description,
-        )
+    )
     embed.set_thumbnail(url=icon)
-    embed.add_field(name= "Server Name" , value = name , inline = False)
+    embed.add_field(name="Server Name", value=name, inline=False)
     embed.add_field(name="Owner", value=owner, inline=True)
     embed.add_field(name="Server ID", value=id, inline=True)
     embed.add_field(name="Region", value=region, inline=True)
     embed.add_field(name="Member Count", value=memberCount, inline=True)
 
     log_channel = client.get_channel(832944803338911765)
-    await log_channel.send(embed=embed) 
-
-
-
+    await log_channel.send(embed=embed)
 
 
 @client.event
-async def on_guild_remove(guild): 
-    with open('prefixes.json', 'r') as f: 
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
         prefixes = json.load(f)
 
-    prefixes.pop(str(guild.id)) 
+    prefixes.pop(str(guild.id))
 
-    with open('prefixes.json', 'w') as f: 
+    with open('prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
-    
 
     name = str(guild.name)
     description = str(guild.description)
@@ -180,61 +232,68 @@ async def on_guild_remove(guild):
     embed = discord.Embed(
         title=" Left a Server ",
         description=description,
-        )
+    )
     embed.set_thumbnail(url=icon)
-    embed.add_field(name= "Server Name" , value = name , inline = False)
+    embed.add_field(name="Server Name", value=name, inline=False)
     embed.add_field(name="Owner", value=owner, inline=True)
     embed.add_field(name="Server ID", value=id, inline=True)
     embed.add_field(name="Region", value=region, inline=True)
     embed.add_field(name="Member Count", value=memberCount, inline=True)
 
     log_channel = client.get_channel(832944803338911765)
-    await log_channel.send(embed=embed) 
+    await log_channel.send(embed=embed)
+
 
 @client.event
 async def on_message(message):
     if client.user == message.author:
         return
 
-    if message.channel.id == 828552656771350528 :
+    if message.channel.id == 828552656771350528:
         async with message.channel.typing():
-             response = await rs.get_ai_response(message.content)
-             await message.reply(response)
+            response = await rs.get_ai_response(message.content)
+            await message.reply(response)
 
     await client.process_commands(message)
 
+
 @client.command(pass_context=True)
-@commands.has_permissions(administrator=True) 
-async def changeprefix(ctx, prefix): 
+@commands.has_permissions(administrator=True)
+async def changeprefix(ctx, prefix):
     with open('prefixes.json', 'r') as f:
         prefixes = json.load(f)
 
     prefixes[str(ctx.guild.id)] = prefix
 
-    with open('prefixes.json', 'w') as f: 
+    with open('prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
 
     await ctx.send(f'Prefix changed to: {prefix}')
 
+
 @client.command(pass_context=True)
 async def meme(ctx):
-    
+
     if not hasattr(client, 'nextMeme'):
         client.nextMeme = getMeme()
 
     name, url = client.nextMeme
-    embed = discord.Embed(title = name)
+    embed = discord.Embed(title=name)
 
     embed.set_image(url=url)
 
     await ctx.send(embed=embed)
-    
+
     client.nextMeme = getMeme()
 
 
 def getMeme():
-    reddit  = praw.Reddit(client_id = "haeimp-lUnhNcw", client_secret = "nZL64R7MdQg8lc0qa3XFtnsnT7zTpQ", username = "0rkx_Bot", password = "Obaid2020", user_agent = "0rxBot")
-    subreddit = reddit.subreddit("meme")   
+    reddit = praw.Reddit(client_id="haeimp-lUnhNcw",
+                         client_secret="nZL64R7MdQg8lc0qa3XFtnsnT7zTpQ",
+                         username="0rkx_Bot",
+                         password="Obaid2020",
+                         user_agent="0rxBot")
+    subreddit = reddit.subreddit("meme")
     all_subs = []
     top = subreddit.top(limit=50)
 
@@ -254,27 +313,45 @@ client.remove_command("help")
 
 @client.group()
 async def help(ctx):
-    embed=discord.Embed(title="Help", url="http://wizardlizard.ml/", description="Help Command", color=0x856319)
+    embed = discord.Embed(title="Help",
+                          url="http://wizardlizard.ml/",
+                          description="Help Command",
+                          color=0x856319)
     embed.set_author(name=f"{ctx.author}")
-    embed.set_thumbnail(url="https://images-ext-1.discordapp.net/external/dqyM9Kbbc5kZri2GcBm87GZsNHwP_rJf-7Z4BPlBVd0/%3Fsize%3D1024/https/cdn.discordapp.com/icons/832934847735005224/092ea39443d2ffe13c3a30dedc61c4cb.webp?width=411&height=411")
-    embed.add_field(name="Economy", value="All Economy Related Commands", inline=True)
-    embed.add_field(name="Configuration", value="Configure the bot", inline=True)
-    embed.add_field(name="Moderation", value="Moderate Your Server", inline=True)
+    embed.set_thumbnail(
+        url=
+        "https://images-ext-1.discordapp.net/external/dqyM9Kbbc5kZri2GcBm87GZsNHwP_rJf-7Z4BPlBVd0/%3Fsize%3D1024/https/cdn.discordapp.com/icons/832934847735005224/092ea39443d2ffe13c3a30dedc61c4cb.webp?width=411&height=411"
+    )
+    embed.add_field(name="Economy",
+                    value="All Economy Related Commands",
+                    inline=True)
+    embed.add_field(name="Configuration",
+                    value="Configure the bot",
+                    inline=True)
+    embed.add_field(name="Moderation",
+                    value="Moderate Your Server",
+                    inline=True)
     embed.add_field(name="Modules", value="Different Modules", inline=True)
-    embed.add_field(name="Information", value="Informative Commands", inline=True)
+    embed.add_field(name="Information",
+                    value="Informative Commands",
+                    inline=True)
     embed.add_field(name="Useful", value="Useful Commands", inline=True)
     embed.add_field(name="Fun", value="Commands to have Fun!", inline=True)
     embed.add_field(name="Reddit", value="Reddit Integreation", inline=True)
-    embed.set_footer(text=f"use help [command] for more information on a command")
+    embed.set_footer(
+        text=f"use help [command] for more information on a command")
     await ctx.send(embed=embed)
+
 
 @help.command()
 async def Economy(ctx):
     await ctx.send("Module Not Added Yet")
 
+
 @help.command()
 async def configuration(ctx):
     await ctx.send("Module Not Added Yet")
+
+
 keep_alive()
-TOKEN = os.environ['TOKEN']
-client.run(TOKEN)
+client.run("ODMyNjkyODU1NzU5MDQ0NjE4.YHnfpw.ub7xAejTjkWCAhLR52mYRLBXjvs")
